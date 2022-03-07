58699164d583d0a3f1b9cac0f5e2fe9e14d127fe
BOJIT <35117353+BOJIT@users.noreply.github.com>
2022-03-07 19:23:56 +0000

    Initial project commit

---
+++ src/lib/core/index.ts
@@ -0,0 +1,7 @@
+import Button from './Button.svelte';
+
+/*----------------------------------------------------------------------------*/
+
+export default {
+	Button: Button
+}
---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Core from '$lib/core/index';
+</script>
+
+<Core.Button color="blue"></Core.Button>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
