a7d611619159bf2efff748740878213b5921085e
BOJIT <35117353+BOJIT@users.noreply.github.com>
2022-03-08 15:21:53 +0000

    Migrate back to Tailwind V2

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,11 @@
 <script>
-    import Button from "$lib/core/Button.svelte";
     import Switch from "$lib/smelte/components/Switch";
+    import Button from "$lib/smelte/components/Button";
 </script>
 
-<h1>This is a Svelte Library: this page is unused.</h1>
-<Button />
 
-<Switch />
+<h3>This is a Svelte Library: this page is unused.</h3>
+
+<Button>Text</Button>
+
+<Switch color={"secondary"}/>
