4225ae9a57db4f01d2cb0fbbe160a886ddbdec5b
BOJIT <35117353+BOJIT@users.noreply.github.com>
2022-03-08 10:17:12 +0000

    Move to Vite builder

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,19 @@
-const path = require('path');
 const preprocess = require('svelte-preprocess');
 
 module.exports = {
-	stories: [
-		"../src/lib/**/*.stories.mdx",
-		"../src/lib/**/*.stories.@(js|jsx|ts|tsx|svelte)"
-	],
-	addons: [
-		"@storybook/addon-links",
-		"@storybook/addon-essentials",
-		"@storybook/addon-svelte-csf"
-	],
-	framework: "@storybook/svelte",
-	svelteOptions: {
-		preprocess: preprocess(),
-	}
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/lib/**/*.stories.@(js|jsx|ts|tsx|svelte)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "framework": "@storybook/svelte",
+  "core": {
+    "builder": "storybook-builder-vite"
+  },
+  "svelteOptions": {
+    "preprocess": preprocess()
+  }
 }
