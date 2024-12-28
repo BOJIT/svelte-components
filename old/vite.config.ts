7b6008635d6c562ef9edf892a9d900087b872eb8
jpedley <james.Bennion-Pedley@dyson.com>
2024-12-28 11:05:34 +0000

    Add more basics

--- old/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
-
-const config: UserConfig = {
-    plugins: [sveltekit()],
-    assetsInclude: ['**/*.gltf', '**/*.glb']
-};
-
-export default config;
---
+++ src/lib/components/ui/link/Link.svelte
@@ -0,0 +1,27 @@
+<!--
+ * @file Link.svelte
+ * @author James Bennion-Pedley
+ * @brief Convenience Wrapper for elements that may be wrapped by links
+ * @date 18/07/2022
+ *
+ * @copyright Copyright (c) 2022
+ *
+-->
+
+<script lang="ts">
+    /*-------------------------------- Imports -------------------------------*/
+
+    import type { HTMLAnchorAttributes } from 'svelte/elements';
+
+    /*--------------------------------- Props --------------------------------*/
+
+    let { href, children, ...rest }: HTMLAnchorAttributes = $props();
+</script>
+
+{#if href !== null}
+    <a {href} {...rest}>
+        {@render children?.()}
+    </a>
+{:else}
+    {@render children?.()}
+{/if}
